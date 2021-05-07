953ad2eec10f8d83eb13e8a945dcda11806fd45f
hannaHayik <79789730+hannaHayik@users.noreply.github.com>
2021-05-07 15:40:30 +0300

    Spam Object
    Contains a word & percentage: percentage expresses the threshold that tells us whether the message is a spam or not along with other parameters

---
+++ Spam.java
@@ -0,0 +1,20 @@
+
+public class Spam {
+
+	private String word;
+	private double percent;
+	
+	public Spam(String word, double percent) {
+		this.word=word;
+		this.percent=percent;
+	}
+	
+	public String getWord() {
+		return this.word;
+	}
+	
+	public double getPercent() {
+		return this.percent;
+	}
+	
+}
